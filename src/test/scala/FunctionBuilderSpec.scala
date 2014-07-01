4eae24c3ba57ca64aa44f9a628107a22e0b0d574
Shokunin San <me@flatmap.io>
2014-07-01 21:53:26 +0200

    Added selector implementation.

--- src/test/scala/FunctionBuilderSpec.scala
+++ src/test/scala/FunctionBuilderSpec.scala
@@ -3,10 +3,28 @@ import functionbuilder.MapKeySelector._
 
 class FunctionBuilderSpec extends FlatSpec with Matchers {
 
-  "firstName selector" should "return a value" in {
+  "A selector" should "not accept an empty parameter list" in {
+    intercept[IllegalArgumentException] {
+      selector()
+    }
+  }
+
+  "KeyMapSelector firstName" should "return some value" in {
     val firstName = selector('name, 'first)
     val person = Map('name -> Map('first -> "shokunin"))
-    firstName(person) should equal("shokunin")
+    firstName(person).get should equal("shokunin")
+  }
+
+  "KeyMapSelector firstName" should "return none" in {
+    val firstName = selector('name, 'first)
+    val person = Map('name -> Map('nick -> "shokunin"))
+    firstName(person) should equal(None)
+  }
+
+  "KeyMapSelector firstName" should "return none too" in {
+    val firstName = selector('name, 'first)
+    val person = Map('nick -> "shokunin")
+    firstName(person) should equal(None)
   }
 
 }
