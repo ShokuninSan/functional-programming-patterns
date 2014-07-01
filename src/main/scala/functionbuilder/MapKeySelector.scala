0834f1a554e2c5a2928025102ad1ff603b156db5
Shokunin San <me@flatmap.io>
2014-07-01 21:53:26 +0200

    Pattern 16: Added selector implementation.

--- src/main/scala/functionbuilder/MapKeySelector.scala
+++ src/main/scala/functionbuilder/MapKeySelector.scala
@@ -1,7 +1,26 @@
 package functionbuilder
 
+import scala.annotation.tailrec
+
 object MapKeySelector {
 
-  def selector(path: Symbol*): (Map[Symbol, Any]) => Option[Any] = ???
+  def selector(path: Symbol*): (Map[Symbol, Any]) => Option[Any] = {
+
+    if (path.length == 0)
+      throw new IllegalArgumentException
+
+    @tailrec
+    def go(path: Seq[Symbol], ds: Map[Symbol, Any]): Option[Any] = {
+      if (path.length == 1)
+        ds.get(path(0))
+      else
+        ds.get(path.head) match {
+          case Some(rest: Map[Symbol, Any]) => go(path.tail, rest)
+          case _ => None
+        }
+    }
+
+    (ds: Map[Symbol, Any]) =>  go(path.toSeq, ds)
+  }
 
 }
--- src/test/scala/FunctionBuilderSpec.scala
+++ src/test/scala/FunctionBuilderSpec.scala
@@ -3,10 +3,34 @@ import functionbuilder.MapKeySelector._
 
 class FunctionBuilderSpec extends FlatSpec with Matchers {
 
-  "firstName selector" should "return a value" in {
+  "A selector" should "not accept an empty parameter list" in {
+    intercept[IllegalArgumentException] {
+      selector()
+    }
+  }
+
+  "MapKeySelector" should "return when value is no Map" in {
+    val firstName = selector('name, 'first)
+    val person = Map('name -> 4711)
+    firstName(person) should equal(None)
+  }
+
+  "MapKeySelector firstName" should "return some value" in {
     val firstName = selector('name, 'first)
     val person = Map('name -> Map('first -> "shokunin"))
-    firstName(person) should equal("shokunin")
+    firstName(person).get should equal("shokunin")
+  }
+
+  "MapKeySelector firstName" should "return none" in {
+    val firstName = selector('name, 'first)
+    val person = Map('name -> Map('nick -> "shokunin"))
+    firstName(person) should equal(None)
+  }
+
+  "MapKeySelector firstName" should "return none too" in {
+    val firstName = selector('name, 'first)
+    val person = Map('nick -> "shokunin")
+    firstName(person) should equal(None)
   }
 
 }
